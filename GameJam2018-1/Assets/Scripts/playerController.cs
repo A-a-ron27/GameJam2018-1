756b861e884a0228aad39266e6aa874e2cc2fbef
gumcel <40838640+mcreektest@users.noreply.github.com>
2018-10-20 19:11:34 -0400

    teleport added

--- GameJam2018-1/Assets/Scripts/playerController.cs
+++ GameJam2018-1/Assets/Scripts/playerController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class playerController : MonoBehaviour {
 
     public float speed = 0.1f;
-
+    public float teleportDist = 0.5f;
     Transform player;
-
+    public int coolDown = 0;
     // Use this for initialization
     void Start () {
         player = GameObject.Find("player").GetComponent<Transform>();
@@ -15,7 +15,8 @@ public class playerController : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-
+        if (coolDown > 0)
+            coolDown--;
 
     }
 
@@ -29,7 +30,7 @@ public class playerController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.A))
         {
-            movement += Vector3.left * speed/1.2f;
+            movement += Vector3.left * speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -37,11 +38,19 @@ public class playerController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.D))
         {
-            movement += Vector3.right * speed/1.2f;
+            movement += Vector3.right * speed;
         }
+        
 
+        player.Translate(movement, Space.World);
 
-
+        movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.Space) && coolDown == 0)
+        {
+            movement += (Vector3.up * teleportDist);
+            coolDown += 200;
+        }
+        player.Translate(movement);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
         //transform.rotation = Quaternion.Euler(0, 0, 90);

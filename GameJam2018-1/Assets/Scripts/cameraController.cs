ff68aac14e2fa86cfaab254ff794c204b83e5fef
Patrick Telling <patricktelling@icloud.com>
2018-10-20 02:17:27 -0400

    Sample Room

---
+++ GameJam2018-1/Assets/Scripts/cameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + offset;
+	}
+}
---
+++ GameJam2018-1/Assets/Scripts/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float speed = 0.1f;
+
+    Transform player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("player").GetComponent<Transform>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += (Vector3.up * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector3.left * speed/1.2f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += Vector3.down * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector3.right * speed/1.2f;
+        }
+
+        player.Translate(movement);
+
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+        //transform.rotation = Quaternion.Euler(0, 0, 90);
+    }
+}

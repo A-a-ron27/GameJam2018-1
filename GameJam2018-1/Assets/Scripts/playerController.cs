92b9ba2fd9b32bd91891485769a9e9c79b7b6308
crenshaw53 <crenshaw.53@osu.edu>
2018-10-21 02:35:57 -0400

    Update GameJam2018-1/Assets/Scripts/playerController.cs

--- GameJam2018-1/Assets/Scripts/playerController.cs
+++ GameJam2018-1/Assets/Scripts/playerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class playerController : MonoBehaviour {
 
     public float speed = 0.1f;
+<<<<<<< HEAD
     public Vector3 newPos;
 
+=======
+    public float teleportDist = 0.5f;
+>>>>>>> 756b861e884a0228aad39266e6aa874e2cc2fbef
     Transform player;
-
+    public int coolDown = 0;
     // Use this for initialization
     void Start () {
         player = GameObject.Find("player").GetComponent<Transform>();
@@ -16,7 +20,8 @@ public class playerController : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-
+        if (coolDown > 0)
+            coolDown--;
 
     }
 
@@ -34,7 +39,7 @@ public class playerController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.A))
         {
-            movement += Vector3.left * speed/1.2f;
+            movement += Vector3.left * speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -42,8 +47,9 @@ public class playerController : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.D))
         {
-            movement += Vector3.right * speed/1.2f;
+            movement += Vector3.right * speed;
         }
+<<<<<<< HEAD
         if (Input.GetKeyDown(KeyCode.Space))
         {
             teleport = Vector3.up * speed * 30;
@@ -52,6 +58,21 @@ public class playerController : MonoBehaviour {
 
         player.position += movement;
         
+=======
+        
+
+        player.Translate(movement, Space.World);
+
+        movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.Space) && coolDown == 0)
+        {
+            movement += (Vector3.up * teleportDist);
+            coolDown += 200;
+        }
+        player.Translate(movement);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+>>>>>>> 756b861e884a0228aad39266e6aa874e2cc2fbef
         //transform.rotation = Quaternion.Euler(0, 0, 90);
     }
 }
